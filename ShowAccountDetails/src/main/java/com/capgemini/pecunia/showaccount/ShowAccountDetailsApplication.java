e332487499e899639deadbeffb2a19c50acbe753
Ritika Agarwal <agarwalritika1999@gmail.com>
2020-04-30 19:35:50 +0530

    First commit

---
+++ ShowAccountDetails/src/main/java/com/capgemini/pecunia/showaccount/ShowAccountDetailsApplication.java
@@ -0,0 +1,13 @@
+package com.capgemini.pecunia.showaccount;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowAccountDetailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShowAccountDetailsApplication.class, args);
+	}
+
+}
---
+++ ShowAccountDetails/src/test/java/com/capgemini/pecunia/showaccount/ShowAccountDetailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capgemini.pecunia.showaccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShowAccountDetailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
